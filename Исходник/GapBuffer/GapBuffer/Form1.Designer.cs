e84cab38928016ea1145f0e098b6958657c46f2b
Alexandr <alexandr-d23@mail.ru>
2020-04-06 17:34:27 +0300

    First

---
+++ Исходник/GapBuffer/GapBuffer/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GapBuffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Copy = new System.Windows.Forms.Button();
+            this.cut = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(117, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(800, 218);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(117, 251);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(800, 226);
+            this.textBox2.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(929, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click_1);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть...";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(12, 87);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(99, 39);
+            this.Copy.TabIndex = 5;
+            this.Copy.Text = "Копировать";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // cut
+            // 
+            this.cut.Location = new System.Drawing.Point(12, 27);
+            this.cut.Name = "cut";
+            this.cut.Size = new System.Drawing.Size(99, 39);
+            this.cut.TabIndex = 6;
+            this.cut.Text = "Вырезать";
+            this.cut.UseVisualStyleBackColor = true;
+            this.cut.Click += new System.EventHandler(this.cut_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(12, 148);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(99, 39);
+            this.Paste.TabIndex = 7;
+            this.Paste.Text = "Вставить";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(12, 206);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(99, 39);
+            this.del.TabIndex = 8;
+            this.del.Text = "Удалить";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(929, 489);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.cut);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button cut;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Исходник/GapBuffer/GapBuffer/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GapBuffer
+{
+    public partial class Form1 : Form
+    {
+        private string currentFile;
+        private char[] str;
+        private int bufSize;
+        private int bufLeft;
+        private int bufRight;
+        private int bufSegment;
+        private int cursor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            createBuffer();
+            textBox1.ContextMenu = new ContextMenu();
+            textBox2.ContextMenu = new ContextMenu();
+            this.Text = "Безымянный";
+        }
+
+        public void createBuffer()
+        {
+            bufLeft = 0;
+            bufSize = 20;
+            bufRight = bufSize - 1;
+            cursor = bufSize;
+            bufSegment = 20;
+            str = new char[1000];
+            for (int i = 0; i < bufSize; i++)
+            {
+                str[i] = '_';
+            }
+            
+            textBox2.Text = new string(str);
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {   
+            int ind = textBox1.SelectionStart;
+            int length = textBox1.Text.Length;
+            char c = e.KeyChar;
+            if (c == (char)Keys.Back && (textBox1.SelectionStart!=0 ||textBox1.SelectionLength!=0))
+            {
+                delete();
+               
+            }
+            if (c == '\r')
+            {   if (textBox1.SelectionLength != 0)
+                {
+                    delete();
+                    length -= textBox1.SelectionLength;
+                }
+                if (ind == length) putLast(Environment.NewLine);
+                else putByIndex(Environment.NewLine, ind);
+            }
+            if (c >= '!' && c <= '~' || c >= 'А' && c <= 'я' || c == ' ' || c == 'Ё' || c == 'ё')
+            {
+                if (textBox1.SelectionLength != 0)
+                {
+                    delete();
+                    length -= textBox1.SelectionLength;
+                }
+                if (ind == length) putLast(Convert.ToString(c));
+                else putByIndex(Convert.ToString(c),ind);
+            }
+             
+        }
+
+        public void delete()
+        {
+
+            if (textBox1.SelectionStart + textBox1.SelectionLength == textBox1.Text.Length && cursor - textBox1.SelectionLength > bufRight)
+            {
+                deleteLast(textBox1.SelectionLength);
+            }
+            else
+            {
+                int index = textBox1.SelectionLength == 0 ? textBox1.SelectionStart - 1 : textBox1.SelectionStart;
+                deleteByIndex(index, textBox1.SelectionLength);
+            }
+
+        }
+
+        //putLast is a method that puts all of the elements at the end of string;
+        private void putLast(string s)
+        {
+            if (cursor + s.Length >= str.Length)
+            {             
+                char[] str1=new char[str.Length*2+s.Length];
+                Array.Copy(str, str1,str.Length);
+                str = str1;                
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                str[cursor] = s[i];
+                cursor++;
+            }
+            textBox2.Text = new string(str);             
+        }
+
+        //deleteLast is a method that deletes the last elements of the string(amount of elements entered by user);
+        private void deleteLast(int count)
+        {   
+            if (count == 0)
+            {
+                str[cursor - 1] = '\0';
+                cursor--;
+                if (str[cursor - 1] == '\r')
+                {
+                    str[cursor - 1] = '\0';
+                    cursor--;
+                }
+            }
+            else
+            {
+                for(int i = 0; i < count; i++)
+                {
+                    str[cursor - 1] = '\0';
+                    cursor--;
+                }
+            }
+            textBox2.Text = new string(str);
+        }
+
+        //deleteByIndex is a method that deletes elements by index (index and amount of elements entered by user);
+        private void deleteByIndex(int index, int count)
+        {
+            moveSpace(distance(index));
+            if (count == 0)
+            {
+                if (str[bufRight + 1] == '\r') str[bufRight + 2] = '_';
+                str[bufRight + 1] = '_';
+                bufRight++;
+                bufSize++;
+            }
+            else
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    str[bufRight + i] = '_';
+                }
+                bufRight+=count;
+                bufSize+=count;
+            }
+            textBox2.Text = new string(str);
+        }
+
+        //putByIndex is a method that puts elements by index(elements and index of insertion entered by user);
+        private void putByIndex(string s,int index)
+        {   
+            moveSpace(distance(index));
+            if (s.Length >= bufSize) expanseBuffer(s.Length);
+            for(int i = 0; i < s.Length; i++)
+            {
+                str[bufLeft] = s[i];
+                bufLeft++;
+                bufSize--;
+            }
+            textBox2.Text = new string(str);
+        }
+
+        //moveSpase is a method that moves the space to certain distance(entered by user);
+        private void moveSpace(int distance)
+        {   
+            if (distance > 0) {
+                for(int i = 0; i < distance; i++)
+                {
+                    str[bufLeft] = str[bufRight + 1];
+                    str[bufRight + 1] = '_';
+                    bufLeft++;
+                    bufRight++;
+                }
+            }
+            else
+            {
+                distance *= -1;
+                for(int i = 0; i < distance; i++)
+                {
+                    str[bufRight] = str[bufLeft - 1];
+                    str[bufLeft - 1] = '_';
+                    bufLeft--;
+                    bufRight--;
+                }
+            }
+        }
+
+        //distance is a method that calculates where buffer should move around
+        private int distance(int index)
+        {
+            if (bufLeft >= index) index = index - bufLeft;
+            else index = index + bufSize - bufRight - 1;
+            return index;
+        }
+
+        //expanseBuffer is a method that expends buffer on a fixed value of segment, when the buffer is overload;
+        private void expanseBuffer(int length)
+        {
+            bufSegment += length;
+            char[] str1 = new char[str.Length + bufSegment];
+            for(int i = 0; i < bufLeft; i++)
+            {
+                str1[i] = str[i];
+            }
+            for(int i = 0; i < bufSegment; i++)
+            {
+                str1[bufLeft + i] = '_';
+            }
+            for(int i = bufLeft; i < cursor; i++)
+            {
+                str1[i + bufSegment] = str[i];
+            }
+            str = str1;
+            bufRight += bufSegment;
+            cursor += bufSegment;
+            bufSize += bufSegment;
+            bufSegment *= 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (textBox1.SelectionLength == 0 || textBox1.Text.Length==0) return;
+            Clipboard.SetText(textBox1.Text.Substring(textBox1.SelectionStart, textBox1.SelectionLength));
+            textBox1.Focus();
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            int start = textBox1.SelectionStart;
+
+            int length = textBox1.SelectionLength;
+            del_Click(sender, e);
+            if (start == textBox1.Text.Length)
+            {
+                putLast(Clipboard.GetText());
+                textBox1.Text=textBox1.Text.Insert(start, Clipboard.GetText());
+            }
+            else
+            {
+                putByIndex(Clipboard.GetText(), start);
+                textBox1.Text=textBox1.Text.Insert(start, Clipboard.GetText());
+                
+            }
+            textBox1.SelectionStart = start + Clipboard.GetText().Length;
+            textBox1.Focus();
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            int start = textBox1.SelectionStart;
+            
+
+            if (textBox1.SelectionLength == 0) return;
+            if (textBox1.SelectionStart + textBox1.SelectionLength == textBox1.Text.Length && cursor - textBox1.SelectionLength > bufRight)
+            {
+                
+                deleteLast(textBox1.SelectionLength);
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - textBox1.SelectionLength);
+                
+            }
+            else
+            {
+                int index = textBox1.SelectionLength == 0 ? textBox1.SelectionStart - 1 : textBox1.SelectionStart;
+                deleteByIndex(index, textBox1.SelectionLength);
+                textBox1.Text = textBox1.Text.Remove(index, textBox1.SelectionLength);
+
+            }
+            textBox1.SelectionStart = start;
+            textBox1.Focus();
+        }
+
+        private void cut_Click(object sender, EventArgs e)
+        {
+            Copy_Click(sender, e);
+            del_Click(sender, e);
+        }
+
+        private void создатьToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            bufLeft = 0;
+            bufSize = 20;
+            bufRight = bufSize - 1;
+            cursor = bufSize;
+            bufSegment = 4;
+            str = new char[100];
+            for (int i = 0; i < bufSize; i++)
+            {
+                str[i] = '_';
+            }
+            this.Text = "Безымянный";
+            textBox1.Text = null;
+            textBox2.Text = new string(str);
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            currentFile = openFileDialog1.FileName;
+            string text = System.IO.File.ReadAllText(currentFile);
+            putLast(text);
+            textBox1.Text = text;
+            MessageBox.Show("Файл открыт");
+            this.Text = openFileDialog1.FileName;
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (currentFile == null)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                currentFile = saveFileDialog1.FileName;
+                System.IO.File.WriteAllText(currentFile, textBox1.Text);
+                MessageBox.Show("Файл сохранен");
+            }
+            else
+            {
+                System.IO.File.WriteAllText(currentFile, textBox1.Text);
+                MessageBox.Show("Файл сохранен");
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            currentFile = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(currentFile, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+            this.Text = saveFileDialog1.FileName;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
